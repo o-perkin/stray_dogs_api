b13ae492d6465360df254eabcfda8c31c1a0af8b
Oleksiy Perkin <perkin.oleksiy@gmail.com>
2020-06-10 14:06:57 +0300

    Added link to header

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :dogs
+  get '/dogs', to: 'dogs#index'
   root to: "dogs#home"
+
   
 end
