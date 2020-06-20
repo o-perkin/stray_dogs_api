3b72a0bd1d61a237edc9cc3ba8de9b9deb8ca195
Oleksiy Perkin <perkin.oleksiy@gmail.com>
2020-06-20 13:49:05 +0300

    Refactored sortable method inside Aplication Helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,24 +7,12 @@ module ApplicationHelper
     return @favorite_exists ? "Remove from favorites" : "Add to favorites"
   end
 
-  def sortable(column, title = nil, page)
-    title ||= column.titleize
-    css_class = column == sort_column ? "flex-sm-fill text-sm-center nav-link active dropdown-toggle" : "flex-sm-fill text-sm-center nav-link"
-    active_class = column == "created_at" && sort_column == "created_at" ? "active" : ""
-    direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
-    link_to title, path_for_sortable(column, direction, title, page), {:class => css_class + " " + active_class, :remote => true}
-  end
-
-  def path_for_sortable(column, direction, title, page)
-    if page == "index"
-      dogs_path(request.parameters.merge({:sort => column, :direction => direction, :page => nil}))
-    else
-      my_list_path(request.parameters.merge({:sort => column, :direction => direction, :page => nil}))
-    end
-  end
-
-  def dropup
-    sort_direction == "desc" ? "dropup" : ""
+  def sortable(column, title, page)
+    direction = define_sorting_direction(column)
+    link_to title, path_for_sortable(column, direction, page), {
+                                                                 :class => define_css_class(column),
+                                                                 :remote => true
+                                                                }
   end
 
   def copyright_generator
@@ -33,9 +21,39 @@ module ApplicationHelper
 
   def access_to_editing(page)
     if page == "index"
-      logged_in?(:site_admin)
+      logged_in?(:site_admin) 
     else
       true
     end
+  end
+
+  private 
+
+  def path_for_sortable(column, direction, page)
+    if page == "index"
+      dogs_path(request.parameters.merge({:sort => column, :direction => direction, :page => nil}))
+    else
+      my_list_path(request.parameters.merge({:sort => column, :direction => direction, :page => nil}))
+    end
+  end
+
+  def define_css_class(column)
+    if column == sort_column
+      "flex-sm-fill text-sm-center nav-link dropdown-toggle active"
+    else
+      "flex-sm-fill text-sm-center nav-link"
+    end
+  end
+
+  def define_sorting_direction(column)
+    if column == sort_column && sort_direction == "asc"
+      "desc"
+    else 
+      "asc"
+    end
+  end
+
+  def dropup
+    sort_direction == "desc" ? "dropup" : ""
   end
 end
