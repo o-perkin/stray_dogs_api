1d55116bc252e7914874d2cdf79ef422b866f8b7
Oleksiy Perkin <perkin.oleksiy@gmail.com>
2020-06-20 14:21:32 +0300

    refactored application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,23 +3,23 @@ module ApplicationHelper
     "active" if current_page? path
   end
 
-  def favorite_text
+  def favorite_text_for_show
     return @favorite_exists ? "Remove from favorites" : "Add to favorites"
-  end
-
-  def sortable(column, title, page)
-    direction = define_sorting_direction(column)
-    link_to title, path_for_sortable(column, direction, page), {
-                                                                 :class => define_css_class(column),
-                                                                 :remote => true
-                                                                }
   end
 
   def copyright_generator
     PerkinViewTool::Renderer.copyright 'Alex Perkin', 'All rights reserved'
   end
 
-  def access_to_editing(page)
+  def sortable(column, title, page)
+    direction = define_sorting_direction(column)
+    link_to title, define_path_for_sortable(column, direction, page), {
+                                                                        class: define_css_class(column),
+                                                                        remote: true
+                                                                      }
+  end
+
+  def define_access_to_edit(page)
     if page == "index"
       logged_in?(:site_admin) 
     else
@@ -27,13 +27,18 @@ module ApplicationHelper
     end
   end
 
+  def favorite_text_for_list(user, dog_id)
+    user.favorites.find_by_dog_id(dog_id) ? "Remove from favorites" : "Add to favorites"
+  end
+
   private 
 
-  def path_for_sortable(column, direction, page)
+  def define_path_for_sortable(column, direction, page)
+    merged_params = request.parameters.merge({sort: column, direction: direction, page: nil})
     if page == "index"
-      dogs_path(request.parameters.merge({:sort => column, :direction => direction, :page => nil}))
+      dogs_path(merged_params)
     else
-      my_list_path(request.parameters.merge({:sort => column, :direction => direction, :page => nil}))
+      my_list_path(merged_params)
     end
   end
 
@@ -53,7 +58,7 @@ module ApplicationHelper
     end
   end
 
-  def dropup
+  def define_dropup_css_class
     sort_direction == "desc" ? "dropup" : ""
   end
 end
