3d397cd2396ce9a7b588cf95e87066345b9153cb
Oleksiy Perkin <perkin.oleksiy@gmail.com>
2020-07-27 13:47:23 +0300

    Refactored favorites controller

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,25 +1,35 @@
 class FavoritesController < ApplicationController
   protect_from_forgery except: :update
+  before_action :set_favorites, only: [:destroy]
+  before_action :set_favorites_by_dog, only: [:update]
 
-  def update
-  
-    favorite = Favorite.where(dog: Dog.find(params[:dog]), user: current_user)
-    if favorite == []
+  def update 
+    
+    if @favorites == []
       Favorite.create(dog: Dog.find(params[:dog]), user: current_user)
       @favorite_exists = true
     else
-      favorite.destroy_all
+      @favorites.destroy_all
       @favorite_exists = false
     end
     respond_to :js
   end
 
-  def destroy
-    favorite = Favorite.find(params[:id])
-    favorite.destroy
+  def destroy    
+    @favorites.destroy
     respond_to do |format|
       format.html { redirect_to my_favorites_path, notice: 'Dog was successfully destroyed.' }
     end
   end
+
+  private
+
+    def set_favorites
+      @favorites = Favorite.find(params[:id])
+    end
+
+    def set_favorites_by_dog
+      @favorites = Favorite.where(dog: Dog.find(params[:dog]), user: current_user)
+    end
 end
  
