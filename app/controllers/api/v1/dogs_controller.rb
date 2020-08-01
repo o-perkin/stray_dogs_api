b054aafaae6be3d0b0af50d924c13ac2c77f00dc
Oleksiy Perkin <perkin.oleksiy@gmail.com>
2020-08-01 10:52:12 +0300

    Moved all controllers to api/v1 folder inside api/v1 modules and added sort_column/sort_direction methods to DogsController from helper

---
+++ app/controllers/api/v1/dogs_controller.rb
@@ -0,0 +1,109 @@
+ module API
+  module V1
+
+     class DogsController < ApplicationController
+      include SendEmails
+      before_action :set_dog, only: [:show, :edit, :update, :destroy]
+      before_action :set_new_dog, only: [:create]
+      access all: [:show, :index, :home], user: :all, site_admin: :all
+
+      # GET /
+      def home
+      end
+     
+      # GET /dogs
+      def index 
+        @dogs = set_list_of_dogs.per(5).search(params[:search])
+      end
+
+      # GET /my_list
+      def my_list
+        @dogs = set_list_of_dogs.per(2).current_user(current_user.id)
+      end
+
+      # GET /my_favorites
+      def favorites
+      end  
+
+      # GET /dogs/1
+      def show   
+        @favorite_exists = Favorite.favorite_exists?(@dog, current_user) 
+      end
+
+      # GET /dogs/new
+      def new
+        @dog = Dog.new
+      end
+
+      # GET /dogs/1/edit
+      def edit
+      end
+
+      # POST /dogs
+      def create  
+        respond_to do |format|
+          if @dog.save
+            send_emails(current_user, set_subscriptions)
+            format.html { redirect_to @dog, notice: 'Dog was successfully created.' }
+          else
+            format.html { render :new }
+          end
+        end
+      end
+
+      # PATCH/PUT /dogs/1
+      def update
+        respond_to do |format|
+          if @dog.update(dog_params)
+            format.html { redirect_to @dog, notice: 'Dog was successfully updated.' }
+          else
+            format.html { render :edit }
+          end
+        end
+      end
+
+      # DELETE /dogs/1
+      def destroy
+        @dog.destroy
+        respond_to do |format|
+          format.html { redirect_to dogs_url, notice: 'Dog was successfully destroyed.' }
+        end
+      end
+
+      private
+     
+        def set_dog
+          @dog = Dog.find(params[:id])
+        end
+
+        def set_new_dog
+          @dog = Dog.new(dog_params)
+          @dog.user_id = current_user.id
+        end
+
+        def set_subscriptions      
+          Subscription.find_by_dog_params(params[:dog])
+        end
+
+        def set_list_of_dogs
+          Dog.filters(set_filter_params).order(sort_column + " " + sort_direction).page(params[:page])
+        end
+
+        def dog_params
+          params.require(:dog).permit(:name, :breed_id, :city_id, :age_id, :description, :user_id)
+        end
+
+        def set_filter_params
+          {breed: params[:breed_id], city: params[:city_id], age_from: params[:age_from], age_to: params[:age_to]}.transform_values {|v| v == "" ? v = nil : v}   
+        end
+
+        def sort_column
+          Dog.column_names.include?(params[:sort]) ? params[:sort] : "created_at"
+        end
+
+        def sort_direction
+          %w[asc desc].include?(params[:direction]) ? params[:direction] : "desc"
+        end
+    end
+  end
+end
---
+++ app/controllers/api/v1/favorites_controller.rb
@@ -0,0 +1,42 @@
+ module API
+  module V1
+
+    class FavoritesController < ApplicationController
+      protect_from_forgery except: :update
+      before_action :set_favorites, only: [:destroy]
+      before_action :find_favorites_by_dog, only: [:update]
+
+      def update     
+        if @favorites == []
+          create_favorite
+          @favorite_exists = true
+        else
+          @favorites.destroy_all
+          @favorite_exists = false
+        end
+        respond_to :js
+      end
+
+      def destroy    
+        @favorites.destroy
+        respond_to do |format|
+          format.html { redirect_to my_favorites_path, notice: 'Dog was successfully destroyed.' }
+        end
+      end
+
+      private
+
+        def set_favorites
+          @favorites = Favorite.find(params[:id])
+        end
+
+        def find_favorites_by_dog
+          @favorites = Favorite.find_by_dog(Dog.find(params[:dog]), current_user)
+        end
+
+        def create_favorite
+          Favorite.create(dog: Dog.find(params[:dog]), user: current_user)
+        end
+    end
+  end
+end
