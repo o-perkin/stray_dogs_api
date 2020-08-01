5834a945b9dc1b3580b4997a8eb354e6c3d5c772
Oleksiy Perkin <perkin.oleksiy@gmail.com>
2020-08-01 10:54:45 +0300

    Put all routes inside namespace api and module blocks

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
 
-  root to: "dogs#home"
+  namespace :api do
+    namespace :v1 do 
 
-  resources :dogs
-  resources :favorites, only: [:destroy]
-  resources :subscribes
-  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
-  get 'favorites/update', defaults: { format: 'js' }
-  get '/my_list', to: 'dogs#my_list'  
-  get 'my_favorites', to: 'dogs#favorites'  
+      root to: "dogs#home"
+
+      resources :dogs
+      resources :favorites, only: [:destroy]
+      resources :subscribes
+      devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
+      get 'favorites/update', defaults: { format: 'js' }
+      get '/my_list', to: 'dogs#my_list'  
+      get 'my_favorites', to: 'dogs#favorites'  
+      
+    end
+  end
 end
