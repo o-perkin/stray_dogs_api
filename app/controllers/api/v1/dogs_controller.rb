5aa6e99c94d9a19facfa18f6e1f00903bbcb7fde
Oleksiy Perkin <perkin.oleksiy@gmail.com>
2020-08-04 08:27:39 +0300

    Moved controllers to api/v1 folder and routes into namespases ap1/v1

---
+++ app/controllers/api/v1/dogs_controller.rb
@@ -0,0 +1,107 @@
+module Api
+  module V1
+    class DogsController < ApplicationController
+      include SendEmails
+      before_action :set_dog, only: [:show, :edit, :update, :destroy]
+      before_action :set_new_dog, only: [:create]
+      before_action :authenticate_user!, only: [:my_list, :create, :update, :destroy]
+
+      # GET /
+      def home
+      end
+
+      # GET /dogs
+      def index 
+        @dogs = set_list_of_dogs.per(5).search(params[:search])
+        render json: {status: "Success",  messsage: "Loaded dogs", data: @dogs}, status: :ok
+      end
+
+      # GET /my_list
+      def my_list
+        @dogs = set_list_of_dogs.per(2).current_user(current_user.id)
+        render json: {status: "Success",  messsage: "Loaded dogs", data: @dogs}, status: :ok
+      end
+
+      # GET /my_favorites
+      def favorites
+      end  
+
+      # GET /dogs/1
+      def show   
+        @favorite_exists = Favorite.favorite_exists?(@dog, current_user)
+        render json: {status: "Success",  messsage: "Loaded dog", data: {dogs: @dog, favorite_exists: @favorite_exists}}, status: :ok 
+      end
+
+      # GET /dogs/new
+      def new
+        @dog = Dog.new
+      end
+
+      # GET /dogs/1/edit
+      def edit
+        render json: {status: "Success",  messsage: "Loaded dog", data: @dog}, status: :ok 
+      end
+
+      # POST /dogs
+      def create  
+        if @dog.save
+          send_emails(current_user, set_subscriptions)
+          render json: {status: "Success",  messsage: "Created a dog", data: @dog}, status: :ok 
+        else
+          render json: {status: "Error",  messsage: "Dog not saved", data: @dog.errors}, status: :unprocessable_entity 
+        end
+      end
+
+      # PATCH/PUT /dogs/1
+      def update      
+        if @dog.update(dog_params)
+          render json: {status: "Success",  messsage: "Updated a dog", data: @dog}, status: :ok 
+        else
+          render json: {status: "Error",  messsage: "Dog not updated", data: @dog.errors}, status: :unprocessable_entity 
+        end      
+      end
+
+      # DELETE /dogs/1
+      def destroy
+        @dog.destroy
+        render json: {status: "Success",  messsage: "Deleted a dog"}, status: :ok
+      end
+
+      private
+
+        def set_dog
+          @dog = Dog.find(params[:id])
+        end
+
+        def set_new_dog
+          @dog = Dog.new(dog_params)
+          @dog.user_id = current_user.id
+        end
+
+        def set_subscriptions      
+          Subscription.find_by_dog_params(params[:dog])
+        end
+
+        def set_list_of_dogs
+          Dog.filters(set_filter_params).order(sort_column + " " + sort_direction).page(params[:page])
+        end
+
+        def dog_params
+          params.require(:dog).permit(:name, :breed_id, :city_id, :age_id, :description, :user_id)
+        end
+
+        def set_filter_params
+          {breed: params[:breed_id], city: params[:city_id], age_from: params[:age_from], age_to: params[:age_to]}.transform_values {|v| v == "" ? v = nil : v}   
+        end
+
+        def sort_column
+          Dog.column_names.include?(params[:sort]) ? params[:sort] : "created_at"
+        end
+
+        def sort_direction
+          %w[asc desc].include?(params[:direction]) ? params[:direction] : "desc"
+        end
+    end
+  end
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,26 @@
 Rails.application.routes.draw do
 
-  root to: "dogs#home"
-
-  resources :dogs
   devise_for :users,
-           path: '',
-           path_names: {
-             sign_in: 'login',
-             sign_out: 'logout',
-             sign_up: 'register'
-           },
-           controllers: {
-             sessions: 'sessions',
-             registrations: 'registrations'
-           }
-  resources :favorites, only: [:destroy]
-  resources :subscribes
-  get 'favorites/update', defaults: { format: 'js' }
-  get '/my_list', to: 'dogs#my_list'  
-  get 'my_favorites', to: 'dogs#favorites'       
-  
+               path: '',
+               path_names: {
+                 sign_in: 'login',
+                 sign_out: 'logout',
+                 sign_up: 'register'
+               },
+               controllers: {
+                 sessions: 'sessions',
+                 registrations: 'registrations'
+               }
+
+  namespace :api do 
+    namespace :v1 do 
+      root to: "dogs#home"
+      resources :dogs      
+      resources :favorites, only: [:destroy]
+      resources :subscribes
+      get 'favorites/update', defaults: { format: 'js' }
+      get '/my_list', to: 'dogs#my_list'  
+      get 'my_favorites', to: 'dogs#favorites' 
+    end
+  end   
 end
